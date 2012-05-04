2ca5029ac9ead0d9c7bd675ee1b5fb6e8d78427c
Chris Lee <sirleech@gmail.com>
2012-05-04 20:32:30 +1000

    python loop

--- log.py
+++ log.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
-#rrdtool update temperature.rrd  N:26
-
 import rrdtool
-rrdtool.update('temperature.rrd','N:22')
+import random
+import time
+
+while (1):
+	value = 'N:'
+	temp = random.randrange(17,29)
+	value += str(temp)
+	rrdtool.update('temperature.rrd',value)
+	print temp	
+	now = int(time.time())
+	range = 240
+	rrdtool.graph('output.png',
+		            '--start', str(now-range), 
+		            '--end', str(now),
+				        'DEF:myspeed=temperature.rrd:temp:AVERAGE',
+		            'LINE2:myspeed#FF0000')              
+	time.sleep(1)
