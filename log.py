d15881797727f438ea02cb1461fc7261d0e21040
Chris Lee <sirleech@gmail.com>
2012-05-11 18:43:07 +1000

    add a filter to remove the bad readings e.g. 1783 degrees c instead of 17.83 degrees c

--- log.py
+++ log.py
@@ -9,13 +9,29 @@ import serial
 oneWeek =  604800
 twelveHours = 43200
 twoHours = 7200
-loggingInterval = 15
+
+# settings
+loggingInterval = 30 #aka 'step'
+filterBracket = 5	
+
+# vars
+lastTemp = 0
+
 
 #infinite loop
 while (1):	
 	try:
 		temp = temperature.getValue()
-		utils.logDataPoint("temperature.rrd",temp)
+		
+		# filter the readings with a specified degrees c bracket
+		if (temp > lastTemp + filterBracket or temp < lastTemp - filterBracket):
+			print 'EXTRANEOUS READING! Reading is greater or less than',filterBracket,'degrees of the previous reading.'
+		else:	
+			utils.logDataPoint("temperature.rrd",temp)
+			# store the last temperature reading
+			lastTemp = temp
+			
+		# export the graphs, json
 		temperature.makePngGraph('Temp C 12 Hours',twelveHours,'web/temperature-12hrs.png')
 		temperature.makePngGraph('Temp C 2 Hours',twoHours,'web/temperature-2hrs.png')
 		temperature.makePngGraph('Temp C 1 Week',oneWeek,'web/temperature-1wk.png')
